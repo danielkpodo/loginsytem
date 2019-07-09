79bfdcffd74f31df3d3dce8a2946e45fce28153b
naphtha <naphthanewman@gmail.com>
2019-07-09 17:41:35 +0000

    added php mysqli function to escape malicious user input

--- inc/form-process.php
+++ inc/form-process.php
@@ -1,6 +1,13 @@
+<?php require_once('./dbconnect.php'); ?>
 <?php
     if (isset($_POST['submit'])) {
+        $user_firstname = mysqli_real_escape_string($conn, $_POST['user_firstname']);
+        $user_lastname = mysqli_real_escape_string($conn, $_POST['user_lastname']);
+        $user_email = mysqli_real_escape_string($conn, $_POST['user_email']);
+        $user_pwd = mysqli_real_escape_string($conn, $_POST['user_pwd']);
+        $user_alias = mysqli_real_escape_string($conn, $_POST['user_alias']);
             
     } else {
         header("Location: ../signup.php");
+            exit();
     }
